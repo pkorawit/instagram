9af55f81cc4a89df7a0cc8c8d402f60d62d44fbe
Korawit <korawit.p@gmail.com>
2022-09-17 09:18:48 +0700

    Change processing style to batch process

--- ads_prediction.py
+++ ads_prediction.py
@@ -6,16 +6,28 @@ from tensorflow import keras
 model_path = './models/Model_ResNet50_C[AdsNoads]_E100B64.h5' 
 model = keras.models.load_model(model_path)
 image_size = (224, 224)
-img = keras.preprocessing.image.load_img(
-    "./photos/CS0f9gyBjOm.jpg", target_size=image_size
-)
-img_array = keras.preprocessing.image.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create batch axis
 
-predictions = model.predict(img_array)
-score = predictions[0]
-print(score)
-print(
-    "This image is %.2f percent Ads and %.2f percent Not Ads."
-    % (score[0] * 100, score[1] * 100)
-)
+def predict(file):
+    img = keras.preprocessing.image.load_img(
+        file, target_size=image_size
+    )
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model.predict(img_array)
+    score = predictions[0]
+    return score
+    
+
+import os
+directory = 'photos'
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        score = predict(f)
+        print(
+        "%s =>  %.2f percent Ads and %.2f percent Not Ads."
+        % (f, score[0] * 100, score[1] * 100)
+        )
+
--- tour_prediction.py
+++ tour_prediction.py
@@ -1,21 +1,32 @@
 
-# Ads or Not Ads Classification
+# Tourist or Non-tourist post classification
 
 import tensorflow as tf
 from tensorflow import keras
 model_path = './models/Model_VGG16_C[TourNotour]_E100B64.h5' 
 model = keras.models.load_model(model_path)
 image_size = (224, 224)
-img = keras.preprocessing.image.load_img(
-    "./photos/CS0f9gyBjOm.jpg", target_size=image_size
-)
-img_array = keras.preprocessing.image.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create batch axis
 
-predictions = model.predict(img_array)
-score = predictions[0]
-print(score)
-print(
-    "This image is %.2f percent tourist-realted and %.2f percent NOT tourist-realted"
-    % (score[0] * 100, score[1] * 100)
-)
+def predict(file):
+    img = keras.preprocessing.image.load_img(
+        file, target_size=image_size
+    )
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model.predict(img_array)
+    score = predictions[0]
+    return score
+    
+
+import os
+directory = 'photos'
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        score = predict(f)
+        print(
+        "%s =>  %.2f percent Tourist and %.2f percent None-Tourist."
+        % (f, score[0] * 100, score[1] * 100)
+        )
