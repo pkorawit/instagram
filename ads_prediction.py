737cedf8dead40dadd47589e950fcba3f79c1014
Korawit <korawit.p@gmail.com>
2022-09-23 10:24:27 +0700

    Run with database update

--- ads_prediction.py
+++ ads_prediction.py
@@ -1,11 +1,20 @@
 
 # Ads or Not Ads Classification
 
+from re import S
 import tensorflow as tf
 from tensorflow import keras
 model_path = './models/Model_ResNet50_C[AdsNoads]_E100B64.h5' 
 model = keras.models.load_model(model_path)
 image_size = (224, 224)
+
+import psycopg2
+conn = psycopg2.connect(
+    host="172.26.117.18",
+    database="postgres",
+    user="postgres",
+    password="zxc123**")
+
 
 def predict(file):
     img = keras.preprocessing.image.load_img(
@@ -17,17 +26,40 @@ def predict(file):
     predictions = model.predict(img_array)
     score = predictions[0]
     return score
+
+def setIsAds(shortcode):
+    try:
+        sql = """ UPDATE sandbox.post SET is_ads=true WHERE shortcode='{}'""".format(shortcode)
+        cur = conn.cursor()
+        # print(sql)
+        cur.execute(sql)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
     
 
 import os
-directory = 'photos'
-for filename in os.listdir(directory):
+directory = 'post_images_sandbox'
+# directory = `I:\\post_images_sandbox\\`
+count = 0
+list =  sorted(os.listdir(directory))
+for filename in list:
+    count = count + 1
     f = os.path.join(directory, filename)
+    shortcode = os.path.splitext(filename)[0]
     # checking if it is a file
     if os.path.isfile(f):
-        score = predict(f)
-        print(
-        "%s =>  %.2f percent Ads and %.2f percent Not Ads."
-        % (f, score[0] * 100, score[1] * 100)
-        )
+        try:
+            score = predict(f)
+            # print(
+            # "%d/%d %s =>  %.2f percent Ads and %.2f percent Not Ads."
+            # % (count, len(list), shortcode, score[0] * 100, score[1] * 100)
+            # )
+            if score[0] > score[1]:
+                print("%d/%d %s => is Ads" % (count, len(list), shortcode))
+                setIsAds(shortcode)
+        except (Exception) as error:
+            print(error)
 
+conn.commit()
+conn.close()
--- tour_prediction.py
+++ tour_prediction.py
@@ -7,6 +7,14 @@ model_path = './models/Model_VGG16_C[TourNotour]_E100B64.h5'
 model = keras.models.load_model(model_path)
 image_size = (224, 224)
 
+import psycopg2
+conn = psycopg2.connect(
+    host="172.26.117.18",
+    database="postgres",
+    user="postgres",
+    password="zxc123**")
+
+
 def predict(file):
     img = keras.preprocessing.image.load_img(
         file, target_size=image_size
@@ -17,16 +25,40 @@ def predict(file):
     predictions = model.predict(img_array)
     score = predictions[0]
     return score
+
+def setIsTourism(shortcode):
+    try:
+        sql = """ UPDATE sandbox.post SET is_tourism=true WHERE shortcode='{}'""".format(shortcode)
+        cur = conn.cursor()
+        # print(sql)
+        cur.execute(sql)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
     
 
 import os
-directory = 'photos'
-for filename in os.listdir(directory):
+directory = 'post_images_sandbox'
+#directory = 'photos'
+count = 0
+list =  sorted(os.listdir(directory))
+for filename in list:
+    count = count + 1
     f = os.path.join(directory, filename)
+    shortcode = os.path.splitext(filename)[0]
     # checking if it is a file
     if os.path.isfile(f):
-        score = predict(f)
-        print(
-        "%s =>  %.2f percent Tourist and %.2f percent None-Tourist."
-        % (f, score[0] * 100, score[1] * 100)
-        )
+        try:
+            score = predict(f)
+            # print(
+            # "%d/%d %s =>  %.2f percent Tourism and %.2f percent None Tourism."
+            # % (count, len(list), shortcode, score[0] * 100, score[1] * 100)
+            # )
+            if score[0] > score[1]:
+                print("%d/%d %s => is Tourism" % (count, len(list), shortcode))
+                setIsTourism(shortcode)
+        except (Exception) as error:
+            print(error)
+
+conn.commit()
+conn.close()
